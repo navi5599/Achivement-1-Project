9f0c5d3c9f19bdb9355469422716153b28cfad4b
navi5599 <ivkelava10@gmail.com>
2022-09-15 21:54:37 +0200

    Updated styling

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 let btn = document.querySelector('.btn_1');
 let btn2 = document.querySelector('.btn_2');
-// let btn3 = document.querySelector('.btn_3');
+let btn3 = document.querySelector('.btn_3');
 
 let modal_container = document.querySelector('.modal_container_1');
 let modal_container2 = document.querySelector('.modal_container_2');
-// let modal_container3 = document.querySelector('.modal_container_3');
+let modal_container3 = document.querySelector('.modal_container_3');
 
 let close_btn = document.querySelector('.close');
 let close_btn2 = document.querySelector('.close2');
+let close_btn3 = document.querySelector('.close3');
 
 let close_icon = document.querySelector('.x_modal');
 let close_icon2 = document.querySelector('.x_modal2');
+let close_icon3 = document.querySelector('.x_modal3');
 
 btn.addEventListener('click', () => {
   modal_container.classList.add('modal_active');
@@ -24,9 +26,11 @@ btn2.addEventListener('click', () => {
   showSlides(slideIndex);
 });
 
-// btn3.addEventListener('click', () => {
-//   modal_container3.classList.add('modal_active');
-// });
+btn3.addEventListener('click', () => {
+  modal_container3.classList.add('modal_active');
+  slideIndex = 7;
+  showSlides(slideIndex);
+});
 
 close_btn.addEventListener('click', () => {
   modal_container.classList.remove('modal_active');
@@ -34,12 +38,18 @@ close_btn.addEventListener('click', () => {
 close_btn2.addEventListener('click', () => {
   modal_container2.classList.remove('modal_active');
 });
+close_btn3.addEventListener('click', () => {
+  modal_container3.classList.remove('modal_active');
+});
 
 close_icon.addEventListener('click', () => {
   modal_container.classList.remove('modal_active');
 });
 close_icon2.addEventListener('click', () => {
   modal_container2.classList.remove('modal_active');
+});
+close_icon3.addEventListener('click', () => {
+  modal_container3.classList.remove('modal_active');
 });
 
 // SLIDER
