e83d07875e5f4d0edd0efe00f55aaa371aa094dc
navi5599 <ivkelava10@gmail.com>
2022-09-16 16:51:23 +0200

    added gulp and gitignore

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
+// SCSS TASK
+
+function scssTask() {
+  return src('sass/style.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// JAVAscript task
+
+function jsTask() {
+  return src('js/app.js', { sourcemaps: true })
+    .pipe(terser())
+    .pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// BROWSERSYNC task
+
+function browsersyncServe(cb) {
+  browsersync.init({
+    server: {
+      baseDir: '.',
+    },
+  });
+  cb();
+}
+
+// RELOAD Browser
+
+function browsersyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
+
+// WATCH task
+
+function watchTask() {
+  watch('*.html', browsersyncReload);
+  watch(
+    ['scss/**/*.scss', 'js/**/*.js'],
+    series(scssTask, jsTask, browsersyncReload)
+  );
+}
+
+exports.default = series(scssTask, jsTask, browsersyncServe, watchTask);
